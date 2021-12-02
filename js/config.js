5cb4aa072c14dc627b6232a4463e93be3bb6a046
stfDeveloper <sstefancosmin@icloud.com>
2021-12-02 15:04:47 +0100

    prompt funzionante+background

---
+++ js/config.js
@@ -0,0 +1,7 @@
+let userInputName= prompt("YOUR NAME");
+let userInputSurname= prompt("YOUR SURNAME");
+let userInputColor= prompt("YOUR FAVORITE COLOR");
+
+
+
+document.getElementById("password").innerHTML=userInputName+userInputSurname+userInputColor
